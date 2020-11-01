b8d165983056f744aa2624d301582bf23b875bb5
Karen355 <karen_gevorkov@mail.ru>
2020-11-01 21:01:40 +0300

    feat: implement functions for the task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (options.separator === undefined ) {
+    options.separator = '+';
+  }
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = '|';
+  }
+  if (options.addition !== undefined && options.additionRepeatTimes !== undefined) {
+    str += (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1) + options.addition;;
+  }
+  if (options.repeatTimes !== undefined) {
+    str = (str+options.separator).repeat(options.repeatTimes - 1) + str;
+    return str;
+  }
+  else {
+    return str+options.addition;
+  }
 };
   
